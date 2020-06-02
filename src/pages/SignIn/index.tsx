b068938e534e7598aeff70866998d1debe020ee5
miroswd <altamirsantos93@hotmail.com>
2020-06-02 19:26:33 -0300

    Configurações Iniciais

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
